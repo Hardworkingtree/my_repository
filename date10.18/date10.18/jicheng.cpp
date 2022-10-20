9c345ff5bf408ae7f0d7389c480370bdc4a40c98
Hardworking tree <805826164@qq.com>
2022-10-20 16:28:41 +0800

    这是修改过后的主文件

--- date10.18/date10.18/jicheng.cpp
+++ date10.18/date10.18/jicheng.cpp
@@ -1,49 +1,8 @@
 #include<iostream>
 using namespace std;
-
-class Animal
-{
-public:
-	virtual void speak()
-	{
-		cout << "动物在说话" << endl;
-	}
-};
-
-class Cat :public Animal
-{
-public:
-	void speak()
-	{
-		cout << "小猫喵喵喵" << endl;
-	}
-};
-
-class Dog :public Animal
-{
-public:
-	void speak()
-	{
-	cout << "小狗汪汪汪" << endl;
-	}
-};
-
-void doSpeak(Animal &a)
-{
-	a.speak();
-}
-
-void test01()
-{
-	Cat c1;
-	doSpeak(c1);
-	Dog d2;
-	doSpeak(d2);
-}
-
 int main()
 {
-	test01();
+	cout << "hello,world" << endl;
 	system("pause");
 	return 0;
 }
